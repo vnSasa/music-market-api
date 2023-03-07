84356d11b905127ed5327999f4f64fd73142b51b
vnSasa <ol.yaremechko@gmail.com>
2023-03-07 11:53:50 +0200

    added correct migrations

--- schema/20230304053712_db.up.sql
+++ schema/20230304053712_db.up.sql
@@ -15,11 +15,11 @@ CREATE TABLE artist
     about_artist varchar(255)
 );
 
-CREATE TABLE music
+CREATE TABLE song
 (
     id serial not null unique,
     artist_id int references artist (id) on delete cascade,
-    name_music varchar(255) not null,
+    name_song varchar(255) not null,
     genre varchar(255) not null,
     second_genre varchar(255),
     year_of_release varchar(255) not null
@@ -29,5 +29,5 @@ CREATE TABLE user_library
 (
     id serial not null unique,
     user_id int references users (id) on delete cascade,
-    music_id int references music (id) on delete cascade
+    song_id int references song (id) on delete cascade
 );
